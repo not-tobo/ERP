7b41510d4316ebc7ba14f2245846446cc106139b
Tobo <not.tobo.dev@gmail.com>
2021-08-05 15:55:26 +0200

    Projektdateien hinzufügen.

---
+++ ERP-Counter/ERP.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RubyButtonAPI;
+using MelonLoader;
+using System.Collections;
+
+namespace ERP_Counter
+{
+    public static class ModInfo
+    {
+        public const string Name = "ERP";
+        public const string Author = "Topi#1337";
+        public const string Version = "0.0.1";
+        public const string DownloadLink = "näh";
+    }
+    public class ERP : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonCoroutines.Start(StartUiManagerInitIEnumerator());
+            MelonLogger.Msg(ConsoleColor.Magenta, "by Topi#1337");
+        }
+
+        private IEnumerator StartUiManagerInitIEnumerator()
+        {
+            while (VRCUiManager.prop_VRCUiManager_0 == null)
+                yield return null;
+
+            ERPISINTHEGAME();
+        }
+
+        public static void ERPISINTHEGAME()
+        {
+            MelonPreferences.CreateCategory(settingsCategory, "ERP");
+            MelonPreferences.CreateEntry<int>(settingsCategory, "ERPcount", 0, "ERP Count");
+            MelonPreferences.CreateEntry<int>(settingsCategory, "uiMenuX", 0, "UI Button X");
+            MelonPreferences.CreateEntry<int>(settingsCategory, "uiMenuY", 1, "UI Button Y");
+            ERPCOUNT = MelonPreferences.GetEntryValue<int>(settingsCategory, "ERPcount");
+            uiMenuX = MelonPreferences.GetEntryValue<int>(settingsCategory, "uiMenuX");
+            uiMenuY = MelonPreferences.GetEntryValue<int>(settingsCategory, "uiMenuY");
+
+            MelonLogger.Msg(ConsoleColor.Magenta, "ERP BUTTON COORDS X: " + uiMenuX + " Y: " + uiMenuY);
+            MelonLogger.Msg(ConsoleColor.Magenta, "YOU START WITH A COUNT OF " + ERPCOUNT + " TODAY. WILL IT INCREASE?");
+
+            ERPBUTTON = new QMSingleButton("ShortcutMenu", uiMenuX, uiMenuY, "ERP: " + ERPCOUNT, YOU_DID_SHAME, "You did ERP? Click This!", null, null);
+        }
+
+        public static void YOU_DID_SHAME()
+        {
+            ERPCOUNT = ERPCOUNT + 1;
+            MelonLogger.Msg(ConsoleColor.Magenta, "SHAME ON YOU!!!! YOU DID " + ERPCOUNT + " TIMES ERP!");
+            MelonPreferences.SetEntryValue(settingsCategory, "ERPcount", ERPCOUNT);
+            MelonPreferences.Save();
+        }
+
+        public override void OnPreferencesSaved()
+        {
+            ERPCOUNT = MelonPreferences.GetEntryValue<int>(settingsCategory, "ERPcount");
+            ERPBUTTON.setButtonText("ERP:" + ERPCOUNT);
+        }
+
+        public static QMSingleButton ERPBUTTON;
+        public static int ERPCOUNT;
+        public static string settingsCategory = "ERP";
+        public static int uiMenuX;
+        public static int uiMenuY;
+    }
+}
---
+++ ERP-Counter/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using System;
+using ERP_Counter;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("ERP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ERP-Counter")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("c92270e4-3e8c-4716-93d1-ebae19a4cf6b")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(ModInfo.Version)]
+[assembly: AssemblyFileVersion(ModInfo.Version)]
+[assembly: MelonColor(ConsoleColor.DarkMagenta)]
+[assembly: MelonInfo(typeof(ERP), ModInfo.Name, ModInfo.Version, ModInfo.Author, ModInfo.DownloadLink)]
+[assembly: MelonGame("VRChat", "VRChat")]
